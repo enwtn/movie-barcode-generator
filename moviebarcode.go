0c2cf82e2319a5f7ee77e8969af01575e8b85c08
Elliott Newton <elliottnewton8@gmail.com>
2019-02-19 17:15:35 +0000

    Other formats (MKV etc.) now supported.
    Re-implemented how frames are selected.
    Removed checks for total number of frames.
    Fixes #1

--- moviebarcode.go
+++ moviebarcode.go
@@ -28,41 +28,34 @@ func main() {
 	flag.IntVar(&numFrames, "numframes", 1920, "number of frames to sample - effectively image width")
 	flag.Parse()
 
-	totalFrames := getNumFrames(*srcFile)
-	if numFrames > totalFrames {
-		log.Fatal("Number of frames selected is greater than number of frames availible in video")
-	}
+	// TODO Check number of frames does not exceed total number of frames
 
 	_, height := getVideoResolution(*srcFile)
-	frameRate := getFrameRate(*srcFile)
 
 	finalPixels = make([][]imageprocess.Pixel, height)
 	for i := range finalPixels {
 		finalPixels[i] = make([]imageprocess.Pixel, numFrames)
 	}
 
-	frames := totalFrames / numWorkers
-	frameGap := totalFrames / numFrames
+	length := getVideoLength(*srcFile)
+	interval := length / float64(numFrames)
+	period := length / float64(numWorkers)
 
 	var wg sync.WaitGroup
 	wg.Add(numWorkers)
 	for i := 0; i < numWorkers; i++ {
-		go calculateColumns(*srcFile, i*frames, (i+1)*frames-1, frameGap, frameRate, &wg)
+		go calculateColumns(*srcFile, float64(i)*period, float64(i+1)*period, interval, &wg)
 	}
 	wg.Wait()
 
 	imageprocess.CreateImage(finalPixels)
 }
 
-func calculateColumns(filename string, start int, end int, frameGap int, frameRate float32, wg *sync.WaitGroup) {
-	for i := start; i < end; i += frameGap {
-		time := float32(i) / frameRate
-		pixels := getFrame(filename, time)
+func calculateColumns(filename string, start float64, end float64, interval float64, wg *sync.WaitGroup) {
+	for i := start; i < end; i += interval {
+		pixels := getFrame(filename, i)
 		for j := range pixels {
-			// don't know why this is neccesary
-			if i/frameGap < len(finalPixels[0]) {
-				finalPixels[j][i/frameGap] = imageprocess.AveragePixels(pixels[j])
-			}
+			finalPixels[j][int(i/interval)] = imageprocess.AveragePixels(pixels[j])
 		}
 
 		updateProgress()
@@ -70,7 +63,7 @@ func calculateColumns(filename string, start int, end int, frameGap int, frameRa
 	wg.Done()
 }
 
-func getFrame(filename string, time float32) [][]imageprocess.Pixel {
+func getFrame(filename string, time float64) [][]imageprocess.Pixel {
 	t := fmt.Sprintf("%f", time)
 	cmd := exec.Command("ffmpeg", "-accurate_seek", "-ss", t, "-i",
 		filename, "-frames:v", "1", "-hide_banner", "-loglevel", "0", "pipe:.bmp")
@@ -89,27 +82,8 @@ func getFrame(filename string, time float32) [][]imageprocess.Pixel {
 	return pixels
 }
 
-func getFrameRate(filename string) float32 {
-	cmd := exec.Command("ffprobe", "-v", "0", "-of", "csv=p=0", "-select_streams", "v:0", "-show_entries", "stream=r_frame_rate", filename)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	parts := strings.Split(strings.TrimRight(out.String(), "\r\n"), "/")
-	numerator, err := strconv.Atoi(parts[0])
-	denominator, err := strconv.Atoi(parts[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	frameRate := float32(float64(numerator) / float64(denominator))
-	return frameRate
-}
-
-func getNumFrames(filename string) int {
-	cmd := exec.Command("ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries",
-		"stream=nb_frames", "-of", "default=nokey=1:noprint_wrappers=1", filename)
+func getVideoLength(filename string) float64 {
+	cmd := exec.Command("ffprobe", "-i", filename, "-show_entries", "format=duration", "-v", "quiet", "-of", "csv=p=0")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
@@ -117,13 +91,12 @@ func getNumFrames(filename string) int {
 		log.Fatal(err)
 	}
 
-	string := strings.TrimRight(out.String(), "\r\n")
-	n, err := strconv.Atoi(string)
+	length, err := strconv.ParseFloat(strings.TrimRight(out.String(), "\r\n"), 64)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return n
+	return length
 }
 
 func getVideoResolution(filename string) (wdith int, height int) {
