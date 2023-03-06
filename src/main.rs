212a3ffa5872f661e85712c05320db6cc07f1741
en3wton <elliottnewton8@gmail.com>
2023-03-06 18:41:22 +0000

    single threaded

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use ffmpeg::media::Type;
 use ffmpeg::software::scaling::{context::Context, flag::Flags};
 use ffmpeg::util::frame::video::Video;
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() -> Result<(), ffmpeg::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     ffmpeg::init().unwrap();
 
     if let Ok(mut ictx) = input(&env::args().nth(1).expect("Cannot open file.")) {
@@ -32,6 +33,7 @@ fn main() -> Result<(), ffmpeg::Error> {
         )?;
 
         let mut frame_index = 0;
+        let mut column_colours: Vec<Vec<Colour>> = Vec::new();
 
         let mut receive_and_process_decoded_frames =
             |decoder: &mut ffmpeg::decoder::Video| -> Result<(), ffmpeg::Error> {
@@ -39,8 +41,12 @@ fn main() -> Result<(), ffmpeg::Error> {
                 while decoder.receive_frame(&mut decoded).is_ok() {
                     let mut rgb_frame = Video::empty();
                     scaler.run(&decoded, &mut rgb_frame)?;
-                    save_file(&rgb_frame, frame_index).unwrap();
-                    frame_index += 1;
+
+                    println!("frame{}", frame_index);
+
+                    column_colours.push(get_avg_colours(&rgb_frame));
+
+                    frame_index +=1 ;
                 }
                 Ok(())
             };
@@ -51,16 +57,78 @@ fn main() -> Result<(), ffmpeg::Error> {
                 receive_and_process_decoded_frames(&mut decoder)?;
             }
         }
+
         decoder.send_eof()?;
         receive_and_process_decoded_frames(&mut decoder)?;
+
+        println!("Saving frame");
+
+        let width = column_colours.len();
+        let height = column_colours[0].len();
+
+        let final_frame = get_final_frame(&column_colours);
+        save_frame(&final_frame, width as u32, height as u32)?;
     }
 
     Ok(())
 }
 
-fn save_file(frame: &Video, index: usize) -> std::result::Result<(), std::io::Error> {
-    let mut file = File::create(format!("frame{}.ppm", index))?;
-    file.write_all(format!("P6\n{} {}\n255\n", frame.width(), frame.height()).as_bytes())?;
-    file.write_all(frame.data(0))?;
+struct Colour {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+fn get_avg_colours(frame: &Video) -> Vec<Colour> {
+    let data = frame.data(0);
+
+    let mut colours: Vec<Colour> = Vec::new();
+
+    for i in (0..data.len()).step_by(3) {
+        let colour = Colour {
+            r: data[i],
+            g: data[i + 1],
+            b: data[i + 2],
+        };
+
+        colours.push(colour);
+    }
+
+    return (0..frame.height() as usize)
+        .into_iter()
+        .map(|i| &colours[i..frame.width() as usize])
+        .map(|colours| {
+            let r = (colours.iter().map(|c| c.r as usize).sum::<usize>() / colours.len()) as u8;
+            let g = (colours.iter().map(|c| c.g as usize).sum::<usize>() / colours.len()) as u8;
+            let b = (colours.iter().map(|c| c.b as usize).sum::<usize>() / colours.len()) as u8;
+
+            return Colour { r, g, b };
+        })
+        .collect();
+}
+
+fn get_final_frame(column_colours: &Vec<Vec<Colour>>) -> Vec<u8> {
+    let width = column_colours.len();
+    let height = column_colours[0].len();
+
+    let mut frame: Vec<u8> = Vec::new();
+
+    for i in 0..height {
+        for j in 0..width {
+            let col = &column_colours[j][i];
+
+            frame.push(col.r);
+            frame.push(col.g);
+            frame.push(col.b);
+        }
+    }
+
+    return frame;
+}
+
+fn save_frame(frame: &Vec<u8>, width: u32, height: u32) -> std::result::Result<(), std::io::Error> {
+    let mut file = File::create("out.ppm")?;
+    file.write_all(format!("P6\n{} {}\n255\n", width, height).as_bytes())?;
+    file.write_all(frame)?;
     Ok(())
 }
